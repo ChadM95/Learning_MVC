4a7fb8c8c2eb255abd2f1776ee0d418628b6fb91
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2024-01-28 20:15:09 +0000

    create layout page and PotatoController

---
+++ Seeds_MVC/Seeds_MVC/Controllers/PotatoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Seeds_MVC.Controllers
+{
+    public class PotatoController : Controller
+    {
+        
+        public IActionResult BlightResistant()
+        {
+            return View();
+        }
+
+        public IActionResult MainCrop()
+        {
+            return View();
+        }
+    }
+}
+
