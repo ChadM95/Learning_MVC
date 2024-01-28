fb5a50057560ddfd90f574d82901c04431b23488
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2024-01-28 18:25:23 +0000

    empty mvc project

---
+++ Seeds_MVC/Seeds_MVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Seeds_MVC.Models;
+
+namespace Seeds_MVC.Controllers;
+
+public class HomeController : Controller
+{ 
+
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+    
+}
+
