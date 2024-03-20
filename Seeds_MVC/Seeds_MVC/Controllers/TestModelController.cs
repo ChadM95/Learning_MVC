ebab083026663c604289644bafd6c8cde24b29e2
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-20 18:54:07 +0000

    add TestModelController

---
+++ Seeds_MVC/Seeds_MVC/Controllers/TestModelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seeds_MVC.Controllers
+{
+    public class TestModelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Seeds_MVC/Seeds_MVC/obj/Debug/net7.0/Seeds_MVC.AssemblyInfo.cs
+++ Seeds_MVC/Seeds_MVC/obj/Debug/net7.0/Seeds_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seeds_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05d67853e822741985dfe892915d82f0ec7bfee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94747af93bbcfc844ee6ed52f4015eab532e660d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seeds_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seeds_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
