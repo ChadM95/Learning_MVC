94747af93bbcfc844ee6ed52f4015eab532e660d
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-20 18:49:27 +0000

    add seeds model class

---
+++ Seeds_MVC/Seeds_MVC/Models/SeedsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Seeds_MVC.Models
+{
+    public class SeedsModel
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Variety { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+
+
+
+    }
+}
