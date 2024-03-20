54ebec122cfcfc2c894f3eb365be034eb8759183
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-20 21:58:04 +0000

    add display list sample view

--- Test_MVC/Controllers/TestController.cs
+++ Test_MVC/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Test_MVC.Models;
 
 namespace Test_MVC.Controllers
 {
@@ -16,5 +17,33 @@ namespace Test_MVC.Controllers
 
             return View(Test);
         }
+    
+        public IActionResult DisplayList()
+        { 
+            //create list of model objects
+            List<TestModel> TestList = new List<TestModel>();
+
+            Models.TestModel Test = new Models.TestModel
+            {
+                Category = "Flowers",
+                Name = "TestFlower",
+                Description = "Great For Testing Models",
+                Amount = 100
+            };
+
+            Models.TestModel Test1 = new Models.TestModel
+            {
+                Category = "Shpuds",
+                Name = "TestShpud",
+                Description = "Great For Getting Spuddy",
+                Amount = 105
+            };
+
+            TestList.Add(Test);
+            TestList.Add(Test1);
+
+            return View(TestList);
+        }
+    
     }
 }
